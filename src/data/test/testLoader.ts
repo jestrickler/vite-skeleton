3870d45b6f3605de42962204c051a00c4a7dc693
strickler.7 <strickler.7@osu.edu>
2025-06-23 16:10:03 -0400

    upgrade react-router-dom

--- src/data/test/testLoader.ts
+++ src/data/test/testLoader.ts
@@ -1,6 +1,4 @@
 import { getTestQuery } from '@/data/test/testApi'
 import { QueryClient } from '@tanstack/react-query'
-import { defer } from 'react-router-dom'
 
-export const loader = async (queryClient: QueryClient) =>
-  defer({ result: queryClient.ensureQueryData(getTestQuery()) })
+export const loader = async (queryClient: QueryClient) => ({ result: queryClient.ensureQueryData(getTestQuery()) })
